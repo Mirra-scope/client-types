324272fe3cb8004aa6aaba8a23facfc30e1404ba
Zohaib Shahid <z-shaid@ejada.com>
2024-01-19 23:02:00 +0500

    feat: financial info optional

--- src/videoHub/FinancialInfoInput.dto.ts
+++ src/videoHub/FinancialInfoInput.dto.ts
@@ -1,11 +1,7 @@
 export interface CreateFinancialInfoInput {
-  NetProfit: number;
-  Budget: number;
-  Revenue: number;
-}
-
-export interface UpdateFinancialInfoInput {
   NetProfit?: number;
   Budget?: number;
   Revenue?: number;
 }
+
+export interface UpdateFinancialInfoInput extends CreateFinancialInfoInput {}
