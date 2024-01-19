f1ffa9e43576272f2496db1d47cd2af0c54feb2a
Zohaib Shahid <z-shaid@ejada.com>
2024-01-20 01:26:20 +0500

    fix: one chge

--- src/videoHub/MovieOutput.dto.ts
+++ src/videoHub/MovieOutput.dto.ts
@@ -11,7 +11,7 @@ export interface GetManagerTableMovieListOutput {
   releaseDate: number;
   imageUrl: string;
   likeCount: number;
-  ratings: number;
+  avarageRating: number;
   uploadDate: number;
 }
 export interface MovieIdOutput {
